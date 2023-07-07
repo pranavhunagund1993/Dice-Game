f9402b6ba770c4c547efac9df280a952502440d8
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-07 17:31:53 +0530

    dice game updated successfully

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// get access to global score elements
+let score0 = document.getElementById('score--0');
+let score1 = document.getElementById('score--1');
+let dice = document.querySelector('.dice');
+
+// Initialize the values to zero
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
+
