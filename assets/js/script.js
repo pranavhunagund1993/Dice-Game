430cfd727f795a0c9d54640d8d567e3fdc9487db
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-22 19:08:55 +0530

    dice game new game button functionality completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,44 @@ let score1 = document.getElementById('score--1');
 let dice = document.querySelector('.dice');
 let rollDice = document.querySelector('.btn--roll');
 let current1 = document.getElementById('current--0');
+let current2 = document.getElementById('current--1');
 let player0 = document.querySelector('.player--0');
 let player1 = document.querySelector('.player--1');
 let holdBtn = document.querySelector('.btn--hold');
 let newGame = document.querySelector('.btn--new');
+let name1 = document.getElementById('name--0');
+let name2 = document.getElementById('name--1');
+
 
 // Initialize the values to zero
-score0.textContent = 0;
-score1.textContent = 0;
-dice.classList.add('hidden');
-
-let scores = [0, 0];
-let current = 0;
-let activePlayer = 0;
+let scores;
+let current;
+let activePlayer;
 
 // scores[activePlayer]
+function init() {
+    score0.textContent = 0;
+    score1.textContent = 0;
+    current1.textContent = 0;
+    current2.textContent = 0;
+
+    name1.textContent = "Player 1";
+    name2.textContent = "Player 2";
+    player0.classList.remove('player--winner');
+    player1.classList.remove('player--winner');
+    player0.classList.add('player--active');
+    player1.classList.remove('player--active');
+    rollDice.classList.remove('hidden');
+    holdBtn.classList.remove('hidden');
+    dice.classList.add('hidden');
+
+
+    scores = [0, 0];
+    current = 0;
+    activePlayer = 0;
+}
+init();
+
 let switchPlayer = function() {
     current = 0;
     document.getElementById(`current--${activePlayer}`).textContent = current;
@@ -71,7 +94,4 @@ holdBtn.addEventListener('click', function() {
 });
 
 // start the new game
-newGame.addEventListener('click', function() {
-    score0.textContent = 0;
-    score1.textContent = 0;
-});
+newGame.addEventListener('click', init);
